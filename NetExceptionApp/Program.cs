638ca4aab659149778530abf87d4bc733b254bd6
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-08-28 19:08:13 +0300

    Добавьте файлы проекта.

---
+++ NetExceptionApp/Program.cs
@@ -0,0 +1,57 @@
+﻿try
+{
+    ParsInt();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Level 0: {ex.Message}");
+}
+
+
+
+void ParsInt()
+{
+    try
+    {
+        string snumber = "123a";
+        int number = Int32.Parse(snumber);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Level 1: {ex.Message}");
+        throw;
+    }
+}
+
+
+
+//string snumber = "123";
+//int number;
+
+//if (Int32.TryParse(snumber, out number))
+//{
+//    Console.WriteLine($"Not error {number}");
+//}
+//else
+//{
+//    Console.WriteLine("Error");
+//}
+
+
+
+//try
+//{
+//    int a = 10, b;
+//    b = Int32.Parse(Console.ReadLine());
+
+//    Console.WriteLine(a / b);
+//}
+//catch(Exception ex)
+//{
+//    Console.WriteLine($"Error: {ex.Message}");
+//    return;
+//}
+//finally
+//{
+//    Console.WriteLine("Finally block");
+//}
